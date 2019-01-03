ea14dbac9e5f1510c59d7a0c3dca978a992c5b14
Phoenix <theemaraldblock@gmail.com>
2019-01-03 19:04:15 +0000

    Update FlufDevSystem.js

--- FlufDevSystem.js
+++ FlufDevSystem.js
@@ -14,6 +14,10 @@ Dev.on('message', message => {
 if (message.author.id === "498186211249225728") {  
   message.channel.send("Hi! This is a temporary message by DogeOS - A V.I.P has not be inputted in please nput the winner so V.I.P Mode can be enabled ")
 message.channel.send("Error - DogeOS")
+	message.channel.send("ERR  System Crash Rebooting DogeOS - PhoenixOS")
+	message.channel.send("ALL PROCESS HANDLED FROM PHOENIXOS")
+message.channel.send("ERR")
+	message.channel.send("ERR ----------------- DogeOS - A System Error Has Fucked up the system please wait while PhoenixOS clears up his shit")
 }
 }
     })
