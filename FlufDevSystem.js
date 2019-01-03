81f88486985ab88fb7adb4bbdb69f9d2ad83484b
Phoenix <theemaraldblock@gmail.com>
2019-01-03 18:55:12 +0000

    Update FlufDevSystem.js

--- FlufDevSystem.js
+++ FlufDevSystem.js
@@ -1,9 +1,21 @@
 const Discord = require('discord.js')
 const Dev = new Discord.Client();
+const prefix = ("!")
+
 
 Dev.on('ready', () => {
 Dev.user.setActivity("Testing new things!")
 Dev.user.setStatus("dnd")
 });
 
+Dev.on('message', message => {
+    if (message.content.startsWith(prefix + "Vip")) {
+	    message.channel.send("Activating V.I.P Mode")
+if (message.author.id === "498186211249225728") {  
+  message.channel.send("Hi! This is a temporary message by DogeOS - A V.I.P has not be inputted in please nput the winner so V.I.P Mode can be enabled ")
+message.channel.send("Error - DogeOS")
+}
+}
+    })
+  
 Dev.login(process.env.FlufDevTK)
