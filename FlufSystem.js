b1635653259266ea737e95e858f5db4cb2b090d5
Phoenix <theemaraldblock@gmail.com>
2019-01-03 19:32:42 +0000

    Update FlufSystem.js

--- FlufSystem.js
+++ FlufSystem.js
@@ -12,10 +12,15 @@ Fluf.on('message', message => {
     if (message.content.startsWith("!Owner")) {
 	    message.channel.send("Activating Owner Mode")
 if (message.author.id === "498186211249225728") {  
-  message.channel.send("Hi and welcome to Fluf Bot Owner Mode! maintained by DogeOS")
-message.channel.send("DogeOS is currently restarting so some commands will be disabled")
-	message.channel.send("Please Wait.")
+  message.channel.send("Hi and welcome to Fluf Bot Owner Mode! maintained by DogeOS Created by ThatPhoenix")
+message.channel.send("The Current Owners of DogeOS is PRESIDENT TRUMP & ThatPhoenix")
+message.channel.send("The commands have not been created yet so fuck off and come back soon")
 }
+	    if (message.author.id === "441980353242988545") {  
+  message.channel.send("Hi and welcome to Fluf Bot Owner Mode! maintained by DogeOS Created by ThatPhoenix")
+message.channel.send("The Current Owners of DogeOS is PRESIDENT TRUMP & ThatPhoenix")
+message.channel.send("The commands have not been created yet so fuck off and come back soon")
 }
+    }
     })
 Fluf.login(process.env.FlufTK)
