964c9a3f39abc916c3bbd0031660fd1ab1cbc11a
Phoenix <theemaraldblock@gmail.com>
2019-01-04 23:15:07 +0000

    Update FlufSystem.js

--- FlufSystem.js
+++ FlufSystem.js
@@ -32,13 +32,19 @@ if (message.author.id === "498186211249225728") {
   message.channel.send("Hi and welcome to Fluf Bot V.I.P Mode! maintained by DogeOS Created by ThatPhoenix")
 message.channel.send("You will soon have BETA Command acess so fuck off and come back soon")
 }
+    })
 	    
-if (message.author.id === "237579126171631617") {  
+Fluf.on('message', message => {
+    if (message.content.startsWith("!Vip")) {   
+	    if (message.author.id === "237579126171631617") {  
   message.channel.send("Hi and welcome to Fluf Bot V.I.P Mode! maintained by DogeOS Created by ThatPhoenix")
 message.channel.send("You will soon have BETA Command acess so fuck off and come back soon")
-}	    
+}	
+    })
 
-if (message.author.id === "441980353242988545") {  
+    Fluf.on('message', message => {
+    if (message.content.startsWith("!Vip")) 
+    {if (message.author.id === "441980353242988545") {  
   message.channel.send("Hi and welcome to Fluf Bot V.I.P Mode! maintained by DogeOS Created by ThatPhoenix")
 message.channel.send("You will soon have BETA Command acess so fuck off and come back soon")
 }
