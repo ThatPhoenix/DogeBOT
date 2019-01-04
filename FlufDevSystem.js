52b5d3d291110040427ade26f346e4da33991eb7
Phoenix <theemaraldblock@gmail.com>
2019-01-04 23:07:48 +0000

    Update FlufDevSystem.js

--- FlufDevSystem.js
+++ FlufDevSystem.js
@@ -8,15 +8,6 @@ Dev.user.setActivity("Testing new things!")
 Dev.user.setStatus("dnd")
 });
 
-Dev.on('message', message => {
-    if (message.content.startsWith(prefix + "Vip")) {
-	    message.channel.send("Activating V.I.P Mode")
-if (message.author.id === "498186211249225728") {  
-  message.channel.send("Hi! This is a temporary message by DogeOS - A V.I.P has not be inputted in please nput the winner so V.I.P Mode can be enabled ")
-message.channel.send("Error - DogeOS")
-	message.channel.send("241563536463486384609806848036360836895383716449617394614 - ERR GREIUQGREUQGRGQIGIRRE - REBOOT_PHOENIXOS_HUIGRIHUGRWHITRUWWIGHUIGHURGHIRUHIRGUWRHGIU")
-}
-}
-    })
+
   
 Dev.login(process.env.FlufDevTK)
