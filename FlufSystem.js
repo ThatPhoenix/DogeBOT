62ab69910132d6bace331f3fb5feb9c917abdd36
Phoenix <theemaraldblock@gmail.com>
2019-01-04 23:07:31 +0000

    Update FlufSystem.js

--- FlufSystem.js
+++ FlufSystem.js
@@ -23,4 +23,32 @@ message.channel.send("The commands have not been created yet so fuck off and com
 }
     }
     })
+
+
+Fluf.on('message', message => {
+    if (message.content.startsWith("!Vip")) {
+	    message.channel.send("Activating V.I.P Mode")
+if (message.author.id === "498186211249225728") {  
+  message.channel.send("Hi and welcome to Fluf Bot V.I.P Mode! maintained by DogeOS Created by ThatPhoenix")
+message.channel.send("You will soon have BETA Command acess so fuck off and come back soon")
+}
+	    
+if (message.author.id === "237579126171631617") {  
+  message.channel.send("Hi and welcome to Fluf Bot V.I.P Mode! maintained by DogeOS Created by ThatPhoenix")
+message.channel.send("You will soon have BETA Command acess so fuck off and come back soon")
+}	    
+
+if (message.author.id === "441980353242988545") {  
+  message.channel.send("Hi and welcome to Fluf Bot V.I.P Mode! maintained by DogeOS Created by ThatPhoenix")
+message.channel.send("You will soon have BETA Command acess so fuck off and come back soon")
+}
+	    
+})
+
+
+
+
+
+
+
 Fluf.login(process.env.FlufTK)
