960e1fb8b3a7ec8105684cda0a99707ab80258c0
Phoenix <theemaraldblock@gmail.com>
2019-04-27 20:55:26 +0100

    Update FlufDevSystem.js

--- FlufDevSystem.js
+++ FlufDevSystem.js
@@ -8,6 +8,17 @@ Dev.user.setActivity("Testing new things!")
 Dev.user.setStatus("dnd")
 });
 
+Dev.on('message', message => {
+    if (message.content.startsWith("!Developer")) {
+	    message.channel.send("Activating Developer Mode")
+if (message.author.id === "498186211249225728") {  
+  message.channel.send("Hello Developer of DogeOS 2.0, DogeOS is completely flawless ")
+message.channel.send("The Only commands you have access to is **!Disconnect** **!Credits** **!DogeShutdown** And more comming soon. ")
+message.channel.send("What Command Would you like to run?")
+}
+    }
+});
+
 
   
 Dev.login(process.env.FlufDevTK)
